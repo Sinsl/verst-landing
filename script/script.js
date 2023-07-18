19c2bd53441a98f63a48a7b633a22483e2ebb16f
Sinsl <sinsl@me.com>
2023-07-18 14:30:58 +0300

    18.07

--- script/script.js
+++ script/script.js
@@ -5,3 +5,12 @@ btnFeedback.addEventListener('click', () => {
   arrFeedback.forEach((item) => item.classList.toggle('feedback-shift'));
 })
 
+const arrFaq = Array.from(document.querySelectorAll('.question'));
+
+arrFaq.forEach(elem => {
+  const span = elem.querySelector('span');
+  span.addEventListener('click', () => {
+    elem.classList.toggle('active-q');
+  })
+})
+
